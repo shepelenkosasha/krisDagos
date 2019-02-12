1ad541d7c782f56d8a825bc2e73ac1d1c8894875
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-02-12 13:17:29 +0200

    Updates

---
+++ precache-manifest.1ec9e1d27f829888c87f1987817c5864.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4194e351be83ff5033734968c037354",
+    "url": "/krisDagos/static/media/slide-9.b4194e35.jpg"
+  },
+  {
+    "revision": "0cd35fa0fafea9497756",
+    "url": "/krisDagos/static/css/main.c4cdb3c4.chunk.css"
+  },
+  {
+    "revision": "9801099b53c93acb47527cac2fc1b918",
+    "url": "/krisDagos/static/media/facebook-logo.9801099b.png"
+  },
+  {
+    "revision": "db3e5f79fa6846f27dcb",
+    "url": "/krisDagos/static/js/1.db3e5f79.chunk.js"
+  },
+  {
+    "revision": "b137d9a33ddd37058e57",
+    "url": "/krisDagos/static/js/runtime~main.b137d9a3.js"
+  },
+  {
+    "revision": "9bddee496b32cc264b46892c267e6790",
+    "url": "/krisDagos/static/media/slide-8.9bddee49.jpg"
+  },
+  {
+    "revision": "840ecf4e7eae9f0772a08df0e322f80a",
+    "url": "/krisDagos/static/media/slide-21.840ecf4e.jpg"
+  },
+  {
+    "revision": "1d9f6428c40c35bcfb3331edfad36e91",
+    "url": "/krisDagos/static/media/slide-15.1d9f6428.jpg"
+  },
+  {
+    "revision": "b8b0ebc40940db24359963e56d00d7e8",
+    "url": "/krisDagos/static/media/about.b8b0ebc4.jpg"
+  },
+  {
+    "revision": "64aa024191637f59d8fa371579a05289",
+    "url": "/krisDagos/static/media/slide-1.64aa0241.jpg"
+  },
+  {
+    "revision": "8402cebed3f6dcde13bca154c729faf9",
+    "url": "/krisDagos/static/media/slide-2.8402cebe.jpg"
+  },
+  {
+    "revision": "677579401f473b68fde9a7ce311d7476",
+    "url": "/krisDagos/static/media/slide-3.67757940.jpg"
+  },
+  {
+    "revision": "1b3f328ab13def3bc72ba8dbe234cd9e",
+    "url": "/krisDagos/static/media/slide-4.1b3f328a.jpg"
+  },
+  {
+    "revision": "34326452800aee79501f62eb2e0a9656",
+    "url": "/krisDagos/static/media/slide-5.34326452.jpg"
+  },
+  {
+    "revision": "52c7f156a40a5b28e66d802bd4819aa6",
+    "url": "/krisDagos/static/media/slide-6.52c7f156.jpg"
+  },
+  {
+    "revision": "179fce6f8bb334153c86032a8169483e",
+    "url": "/krisDagos/static/media/slide-7.179fce6f.jpg"
+  },
+  {
+    "revision": "0cd35fa0fafea9497756",
+    "url": "/krisDagos/static/js/main.0cd35fa0.chunk.js"
+  },
+  {
+    "revision": "f6e70d0bb7a45021fb424a5624e16f31",
+    "url": "/krisDagos/static/media/slide-10.f6e70d0b.jpg"
+  },
+  {
+    "revision": "d0c53b71b7c2f3ad38ca83c0321d507c",
+    "url": "/krisDagos/static/media/slide-11.d0c53b71.jpg"
+  },
+  {
+    "revision": "783be38443517e26d3c505882c6a0107",
+    "url": "/krisDagos/static/media/slide-12.783be384.jpg"
+  },
+  {
+    "revision": "694620b7f3b7f3bb3a89806a72896576",
+    "url": "/krisDagos/static/media/slide-13.694620b7.jpg"
+  },
+  {
+    "revision": "121fbb37c1cccaf88f7ba4dcfa96350a",
+    "url": "/krisDagos/static/media/slide-14.121fbb37.jpg"
+  },
+  {
+    "revision": "e38bb625ed19ce65f96f38209de3af52",
+    "url": "/krisDagos/static/media/slide-16.e38bb625.jpg"
+  },
+  {
+    "revision": "ffb13f204b7ac65800d38c6a447c1174",
+    "url": "/krisDagos/static/media/slide-17.ffb13f20.jpg"
+  },
+  {
+    "revision": "094fd8db9588c833a5a2485e9ad7cc86",
+    "url": "/krisDagos/static/media/slide-18.094fd8db.jpg"
+  },
+  {
+    "revision": "43daf472443d60e3a9cbc3da77758d8e",
+    "url": "/krisDagos/static/media/slide-19.43daf472.jpg"
+  },
+  {
+    "revision": "afb4e1e4fdeecb069947e469a0a9d445",
+    "url": "/krisDagos/static/media/slide-20.afb4e1e4.jpg"
+  },
+  {
+    "revision": "ff389461e6ab7a72e36f2d7f19ddc48e",
+    "url": "/krisDagos/static/media/phone-logo.ff389461.png"
+  },
+  {
+    "revision": "87d55e2ff985b6b35d9758d579fbea80",
+    "url": "/krisDagos/static/media/Gmail-icon.87d55e2f.png"
+  },
+  {
+    "revision": "f38ff40f383af67d9525c1709e60da94",
+    "url": "/krisDagos/static/media/instagram.f38ff40f.png"
+  },
+  {
+    "revision": "db3e5f79fa6846f27dcb",
+    "url": "/krisDagos/static/css/1.49189446.chunk.css"
+  },
+  {
+    "revision": "e56a7a98cf5e357f45d3a37a2aec1138",
+    "url": "/krisDagos/index.html"
+  }
+];
